27b38524023b8b849493afaef4a78f151ee2dcdc
Jamie Magee <jamie.magee@gmail.com>
2018-10-17 11:25:13 +0200

    Static imports

--- src/main/kotlin/dk/jamiemagee/swissspoon/Swissspoon.kt
+++ src/main/kotlin/dk/jamiemagee/swissspoon/Swissspoon.kt
@@ -3,6 +3,8 @@ package dk.jamiemagee.swissspoon
 import mu.KotlinLogging
 import org.jline.utils.AttributedString
 import org.jline.utils.AttributedStyle
+import org.jline.utils.AttributedStyle.DEFAULT
+import org.jline.utils.AttributedStyle.YELLOW
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
@@ -17,7 +19,7 @@ class Swissspoon {
 
     @Bean
     fun promptProvider() = PromptProvider {
-        AttributedString("swissspoon:>", AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW))
+        AttributedString("swissspoon:>", DEFAULT.foreground(YELLOW))
     }
 }
 
